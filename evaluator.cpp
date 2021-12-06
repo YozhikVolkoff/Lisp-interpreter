6160ce1df55b01f1d2bac50baa8065819f968d62
Vasyuk Vladimir <84467934+YozhikVolkoff@users.noreply.github.com>
2021-12-06 22:54:28 +0300

    Update evaluator.cpp

--- evaluator.cpp
+++ evaluator.cpp
@@ -1,8 +1,5 @@
-#include some_libs
 
-using std::Node;
-using std::Name;
-using std::NameType;
+
 
 void Evaluator::eval(Node* curr)
 {
@@ -10,49 +7,113 @@ void Evaluator::eval(Node* curr)
     // because the returned type from eval may be s_expresssion 
     // and evel list with its parameters and command.
     // +1 for return
-    list m_results = new list<Node*>;
-    for (auto i = cur->m_chld.begin(); i != cur->m_chld.end(); i++)
+    list<Node*> m_result;
+    for (auto i = cur->m_chld.begin(); i != cur->m_chld.end(); ++i)
     {
-        m_results[i] = eval(i);
-        if (m_results[i] == nullptr)
-        {
-            delete [] m_results;
+        m_result.push_back(eval(i));
+        
+        // If nullptr returned, the error occurs
+        if (!m_result.back()) {
+            m_result.clear();
             return nullptr;
         }
     }
     
-    // Check: the first in list_chld must be operator
-    // if (!findname(. ...  ))
-    /* if (curr->m_name->m_type != OP)
-    {
-        delete [] m_results;
-        // TODO: error message
-        return nullptr;
-    }*/ 
-    
     // Do operation
-    switch(m_results[0]->m_name) {
+    switch(curr->m_name->m_str) {
     case "+":
         bool flag = 0;
-        for (auto i = cur->m_chld.begin(); i != cur->m_chld.end(); i++) if(i->m_name->m_type != NUM) flag = true;
-        
-        // Any child has not type number
-        if (flag)
-        {
-            return nullptr;
-            break;
+        for (auto i = m_result.begin(); i != m_result.end(); ++i) {
+            if(i->m_name->m_type != NUM) flag = true;
         }
+        if (flag) {
+            m_result.clear();
+            return nullptr;
+        }
+           
+        // If all results are NUM, it's possible to sum them
+        Node* t = new Node;
+        t->m_name = new Name;
+        t->m_name->m_value = 0.0;
+        t->m_name->m_type = NameType::NUM;
         
-    
-    
-    
-    
-    // All childs were evaluated; time to eval function in the head
-    // of cur to all its arguements
-    
-   
+        for (auto i = m_result.begin(); i != m_result.end(); ++i) {
+            t->m_name->m_value += i->m_name->m_value;
+        }
             
-            
-            
+        return t;
     
+    case "-":
+        bool flag = 0;
+        for (auto i = m_result.begin(); i != m_result.end(); ++i) {
+            if(i->m_name->m_type != NUM) flag = true;
+        }
+        if (flag) {
+            m_result.clear();
+            return nullptr;
+        }
+           
+        // If all results are NUM, it's possible to sum them
+        Node* t = new Node;
+        t->m_name = new Name;
+        t->m_name->m_value = 0.0;
+        t->m_name->m_type = NameType::NUM;
+        
+        for (auto i = m_result.begin(); i != m_result.end(); ++i) {
+            t->m_name->m_value -= i->m_name->m_value;
+        }
+            
+        return t;
+    
+    case "*":
+        bool flag = 0;
+        for (auto i = m_result.begin(); i != m_result.end(); ++i) {
+            if(i->m_name->m_type != NUM) flag = true;
+        }
+        if (flag) {
+            m_result.clear();
+            return nullptr;
+        }
+           
+        // If all results are NUM, it's possible to sum them
+        Node* t = new Node;
+        t->m_name = new Name;
+        t->m_name->m_value = 0.0;
+        t->m_name->m_type = NameType::NUM;
+        
+        for (auto i = m_result.begin(); i != m_result.end(); ++i) {
+            t->m_name->m_value *= i->m_name->m_value;
+        }
+            
+        return t;
+    
+    case "/":
+        bool flag = 0;
+        for (auto i = m_result.begin(); i != m_result.end(); ++i) {
+            if(i->m_name->m_type != NUM) flag = true;
+        }
+        if (flag) {
+            m_result.clear();
+            return nullptr;
+        }
+           
+        // If all results are NUM, it's possible to sum them
+        Node* t = new Node;
+        t->m_name = new Name;
+        t->m_name->m_value = 0.0;
+        t->m_name->m_type = NameType::NUM;
+        
+        for (auto i = m_result.begin(); i != m_result.end(); ++i) {
+            t->m_name->m_value /= i->m_name->m_value;
+        }
+            
+        return t;
+            
+   case "print":
+        
+        // Simple print for just value in the first child node
+        cout << curr->m_name->m_value << '\n' << endl;
+            
+        return nullptr;
+        
 }
